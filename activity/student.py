23a2b2d1bfed67dbe994ef6c5f3dd3dfcc838020
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-03-27 11:09:04 -0700

    implements class Student

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,18 @@
 # add your Student class here!
+class Student:
+    def __init__(self, name, grade_level, classes):
+        self.name = name 
+        self.grade_level = grade_level
+        self.classes = classes
+    def add_class(self, new_class):
+        self.classes.append(new_class)
+    
+    def get_num_classes(self):
+        return len(self.classes)
+    
+    def summary(self):
+        return f"{self.name} is a {self.grade_level} enrolled in {self.get_num_classes} classes"
+    
+samara = Student("Samara", "junior", [ "Pre-Calc", "English III", "World History", "Gym", "Chemistry", "Music Composition" ])
+
+print(samara.get_num_classes())
